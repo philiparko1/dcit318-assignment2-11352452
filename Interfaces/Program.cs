eb0484726376ba9a76f6525b4f746a851ceafc7b
philiparko <layricky51@gmail.com>
2025-08-01 16:27:41 +0000

    Implemented Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+interface IMoveable {
+    void Move();
+}
+
+class Car : IMoveable {
+    public void Move() => Console.WriteLine("Car is moving");
+}
+
+class Bicycle : IMoveable {
+    public void Move() => Console.WriteLine("Bicycle is moving");
+}
+
+class Program {
+    static void Main() {
+        IMoveable car = new Car();
+        IMoveable bike = new Bicycle();
+
+        car.Move();   // Car is moving
+        bike.Move();  // Bicycle is moving
+    }
+}
