451f5014bd8ff4793041aee62699a99419445a5e
philiparko <layricky51@gmail.com>
2025-08-01 16:25:26 +0000

    Implemented Inheritance and Method Overriding

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Animal {
+    public virtual void MakeSound() => Console.WriteLine("Some generic sound");
+}
+
+class Dog : Animal {
+    public override void MakeSound() => Console.WriteLine("Bark");
+}
+
+class Cat : Animal {
+    public override void MakeSound() => Console.WriteLine("Meow");
+}
+
+class Program {
+    static void Main() {
+        Animal generic = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        generic.MakeSound(); // Some generic sound
+        dog.MakeSound();     // Bark
+        cat.MakeSound();     // Meow
+    }
+}
